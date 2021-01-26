5b1174aee5f6ebe917500b11698fafbd67610e9a
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2021-01-26 11:34:13 +0100

    Added german language

--- lang/lang.lua
+++ lang/lang.lua
@@ -2,5 +2,6 @@ return {
   { id = "en_us", name = "English" },
   { id = "zh_cn", name = "简体中文 (Simplified Chinese)" },
   { id = "zh_tw", name = "正體中文 (Traditional Chinese)" },
-  { id = "ja_jp", name = "日本語 (Japanese)" }
+  { id = "ja_jp", name = "日本語 (Japanese)" },
+  { id = "de_de", name = "Deutsch (German)"}
 }
