958f0ddf78faf1999fdc3079e0c459e5107e87e5
Nats-ji <angel_tommy@foxmail.com>
2021-02-01 09:30:24 +0800

    Fixed for sandbox update

---
+++ scripts/autoexec.lua
@@ -0,0 +1,12 @@
+json = require 'json/json'
+ImGui.CETMM = require 'cet_mod_manager'
+
+Game['DebugGiveHotkeys'] = Game['DebugGiveHotkeys;GameInstance']
+Game['DebugNPCs_NonExec'] = Game['DebugNPCs_NonExec;GameInstanceStringStringString;GameInstance']
+Game['GetPlayer'] = Game['GetPlayer;GameInstance']
+Game['GetPlayerObject'] = Game['GetPlayerObject;GameInstance']
+Game['PlayFinisher'] = Game['PlayFinisher;GameInstance']
+Game['PlayFinisherSingle'] = Game['PlayFinisherSingle;GameInstance']
+Game['PPS'] = Game['PPS;GameInstance']
+
+print("Cyber Engine Tweaks startup complete.")
---
+++ scripts/cet_mod_manager/init.lua
@@ -0,0 +1,104 @@
+local CETMM = {}
+
+local function file_exists(name)
+   local f=io.open(name,"r")
+   if (f~=nil) then io.close(f) return true else return false end
+end
+
+local function getModsPath()
+  local modspath
+  if  file_exists("./cet_mod_manager/init.lua") then
+    modspath = ".\\"
+  elseif file_exists("./bin/x64/plugins/cyber_engine_tweaks/mods/cet_mod_manager/init.lua") then
+    modspath = ".\\bin\\x64\\plugins\\cyber_engine_tweaks\\mods\\"
+  elseif file_exists("./plugins/cyber_engine_tweaks/mods/cet_mod_manager/init.lua") then
+    modspath = ".\\plugins\\cyber_engine_tweaks\\mods\\"
+  end
+  return modspath
+end
+
+local function scan_mods()
+  local ModsPath = getModsPath()
+	local i = 0
+	local mod_list = {}
+	local dir_list = io.popen("dir "..ModsPath.." /b /ad")
+	for dir in dir_list:lines() do
+		i = i + 1
+		mod_list[i] = {
+      name = dir,
+      path = ModsPath..dir
+    }
+	end
+	dir_list:close()
+	return mod_list
+end
+
+local function check_mod_state(modpath)
+	local initpath = modpath.."\\init.lua"
+	local init_disabledpath = modpath.."\\init.lua_disabled"
+	if file_exists(initpath) then
+		return true
+	elseif not file_exists(initpath) and file_exists(init_disabledpath) then
+		return false
+	else
+		return nil
+	end
+end
+
+function CETMM.GetModsData()
+	local mods_data = {}
+	local modList = scan_mods()
+	for i in pairs(modList) do
+		local mod_entry = {}
+		mod_entry.name = modList[i].name
+    mod_entry.path = modList[i].path
+		mod_entry.state = check_mod_state(modList[i].path)
+		table.insert(mods_data, mod_entry)
+	end
+	return mods_data
+end
+
+-- function CETMM.GetDofilesData()
+--   local ModsPath = getModsPath()
+-- 	local dofile_names = {}
+-- 	local dofile_list = io.popen("dir "..ModsPath.."cet_mod_manager\\dofiles\\ /b /a-d")
+-- 	for lua in dofile_list:lines() do
+-- 		if lua:match("(.+)%.lua$") then
+-- 			table.insert(dofile_names, lua)
+-- 		end
+-- 	end
+-- 	dofile_list:close()
+-- 	return dofile_names
+-- end
+
+function CETMM.ToggleMod(mod_entry)
+  local mod_name = mod_entry.name
+  local mod_path = mod_entry.path
+  local enable = mod_entry.state
+	if enable then
+		local ok = os.rename(mod_path.."/init.lua_disabled", mod_path.."/init.lua")
+		-- if ok then
+		-- 	print(i18n("console_msg_mod_enable", { modname = modNameConvert(mod) }))
+		-- else
+		-- 	print(i18n("console_msg_mod_enable_error", { modname = modNameConvert(mod) }))
+		-- end
+	elseif not enable then
+		local ok = os.rename(mod_path.."/init.lua", mod_path.."/init.lua_disabled")
+		-- if ok then
+		-- 	print(i18n("console_msg_mod_disable", { modname = modNameConvert(mod) }))
+		-- else
+		-- 	print(i18n("console_msg_mod_enable_error", { modname = modNameConvert(mod) }))
+		-- end
+	end
+end
+
+function CETMM.OpenFolder(folder)
+  local ModsPath = getModsPath()
+  if folder == "mods" then
+    os.execute("start explorer "..ModsPath)
+  elseif folder == "dofiles" then
+    os.execute("start explorer "..ModsPath.."cet_mod_manager\\dofiles")
+  end
+end
+
+return CETMM
