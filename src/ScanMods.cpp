f36d542b3c5003188474676e3253f1008f52b1e1
nat-js <Angel_tommy@foxmail.com>
2021-08-25 08:26:41 +0800

    Add scan cet mod

--- src/ScanMods.cpp
+++ src/ScanMods.cpp
@@ -7,6 +7,7 @@ void ScanMods::Scan()
   m_modlistPath = CETMM::GetPaths().CETMMRoot() / "mod_list.json";
   m_archive = scan_mods(CETMM::GetPaths().Archives(), ".archive");
   m_asi = scan_mods(CETMM::GetPaths().Plugins(), ".asi");
+  m_cet = scan_mods(CETMM::GetPaths().CETMods(), "init.lua", true, true);
   m_red4ext = scan_mods(CETMM::GetPaths().Red4Ext(), ".dll");
   m_redscript = scan_mods(CETMM::GetPaths().RedScript(), ".reds", true);
   save_modlist();
@@ -18,7 +19,7 @@ void ScanMods::Shutdown()
     spdlog::error("Failed to remove {}.", (m_modlistPath).string());
 }
 
-std::vector<std::string> ScanMods::scan_mods(const std::filesystem::path aPath, const std::string aExtension, bool aCheckFolder)
+std::vector<std::string> ScanMods::scan_mods(const std::filesystem::path aPath, const std::string aExtension, bool aCheckFolder, bool aCET)
 {
   std::vector<std::string> mod_list;
   for (const auto& entry : std::filesystem::directory_iterator(aPath))
@@ -27,14 +28,19 @@ std::vector<std::string> ScanMods::scan_mods(const std::filesystem::path aPath, 
     {
       for (const auto& sub_entry : std::filesystem::directory_iterator(entry.path()))
       {
-        if (sub_entry.is_regular_file() && sub_entry.path().extension() == aExtension)
+        if (aCET && sub_entry.is_regular_file() && sub_entry.path().filename() == aExtension)
+        {
+          mod_list.push_back(entry.path().filename().string());
+          break;
+        }
+        else if (sub_entry.is_regular_file() && sub_entry.path().extension() == aExtension)
         {
           mod_list.push_back(entry.path().filename().string());
           break;
         }
       }
     }
-    else if (entry.is_regular_file() && entry.path().extension() == aExtension)
+    else if (!aCET && entry.is_regular_file() && entry.path().extension() == aExtension)
       mod_list.push_back(entry.path().filename().string());
   }
   return mod_list;
@@ -45,6 +51,7 @@ void ScanMods::save_modlist()
   nlohmann::json j_modlist;
   j_modlist["archive"] = m_archive;
   j_modlist["asi"] = m_asi;
+  j_modlist["cet"] = m_cet;
   j_modlist["red4ext"] = m_red4ext;
   j_modlist["redscript"] = m_redscript;
 
--- src/ScanMods.h
+++ src/ScanMods.h
@@ -10,9 +10,10 @@ private:
   std::filesystem::path m_modlistPath;
   std::vector<std::string> m_archive;
   std::vector<std::string> m_asi;
+  std::vector<std::string> m_cet;
   std::vector<std::string> m_red4ext;
   std::vector<std::string> m_redscript;
 
-  std::vector<std::string> scan_mods(const std::filesystem::path aPath, const std::string aExtension, bool aCheckFolder = false);
+  std::vector<std::string> scan_mods(const std::filesystem::path aPath, const std::string aExtension, bool aCheckFolder = false, bool aCET = false);
   void save_modlist();
 };
