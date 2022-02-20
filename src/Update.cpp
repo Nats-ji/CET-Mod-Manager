f1f80a984154a9d752c1530a631cadd7eb82d431
nat-js <Angel_tommy@foxmail.com>
2022-02-20 17:33:10 +0800

    pseudo update codes

--- src/Update.cpp
+++ src/Update.cpp
@@ -11,7 +11,32 @@ void Update::LogVersion()
   }
 }
 
+void Update::RemoveOldFiles()
+{
+  if (!exists(CETMM::GetPaths().Config())) return;
+
+  const std::string last_version = getLastVersion();
+
+  if (last_version == m_version) return;
+
+  // remove old files somehow
+  
+}
+
 const std::string Update::GetVersion() const
 {
   return m_version;
+}
+
+std::string Update::getLastVersion()
+{
+  std::string version;
+  std::ifstream config_file(CETMM::GetPaths().Config());
+  if (config_file)
+  {
+    nlohmann::json j = nlohmann::json::parse(config_file);
+    version = j.value("version", version);
+  }
+  config_file.close();
+  return version;
 }
