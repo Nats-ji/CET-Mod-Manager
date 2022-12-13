ea2f8a6a5fb3e69f5a39fb1237dba45bf32d7576
Mingming Cui <nats_ji@hotmail.com>
2022-12-14 03:02:57 +0800

    show correct version number in red4ext

--- src/CETMM/dllmain.cpp
+++ src/CETMM/dllmain.cpp
@@ -1,4 +1,6 @@
 #include "pch.h"
+#include "version.h"
+#include <regex>
 #include "EXT/TypeRegister.h"
 
 static void Initialize()
@@ -32,6 +34,29 @@ bool BaseInit_OnUpdate(RED4ext::CGameApplication* aApp)
 bool BaseInit_OnExit(RED4ext::CGameApplication* aApp)
 {
     return true;
+}
+
+RED4ext::v0::SemVer CETMM_Version()
+{
+    std::string version = CETMM_VERSION;
+    std::regex rgx(R"((\d+)\.(\d+)\.(\d+)-*(\d+)*(?=-|$))");
+    std::smatch matches;
+
+    if(std::regex_search(version, matches, rgx)) {
+        uint8_t major = std::stoi(matches[1].str());
+        uint16_t minor = std::stoi(matches[2].str());
+        uint32_t patch = std::stoi(matches[3].str());
+        uint32_t prereleaseType = RED4EXT_V0_SEMVER_PRERELEASE_TYPE_NONE;
+        uint32_t prereleaseNumber = 0;
+        
+        if (matches[4].length() != 0) {
+            prereleaseType = RED4EXT_V0_SEMVER_PRERELEASE_TYPE_RC;
+            prereleaseNumber = std::stoi(matches[4].str());
+        }
+        return RED4EXT_SEMVER_EX(major, minor, patch, prereleaseType, prereleaseNumber);
+    } else {
+        return RED4EXT_SEMVER(1, 0, 0);
+    }
 }
 
 RED4EXT_C_EXPORT void RED4EXT_CALL RegisterTypes()
@@ -79,7 +104,7 @@ RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo* aInfo)
 {
     aInfo->name = L"CET Mod Manager";
     aInfo->author = L"Ming";
-    aInfo->version = RED4EXT_SEMVER(1, 0, 0); // Set your version here.
+    aInfo->version = CETMM_Version();
     aInfo->runtime = RED4EXT_RUNTIME_LATEST;
     aInfo->sdk = RED4EXT_SDK_LATEST;
 }
